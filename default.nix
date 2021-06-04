17cc669656e8605005048404bb58a70c3ab57eff
Ron B <me@ronthecookie.me>
2021-06-04 10:13:46 +0300

    nix: fix better-sqlite3 breaking at runtime

--- default.nix
+++ default.nix
@@ -1,10 +1,24 @@
 with (import <nixpkgs> { });
 
-mkYarnPackage {
+let
+  nodeSources = runCommand "node-sources" { } ''
+    tar --no-same-owner --no-same-permissions -xf "${nodejs.src}"
+    mv node-* $out
+  '';
+in mkYarnPackage {
   name = "comicfury-discord-webhook";
   src = ./.;
   packageJSON = ./package.json;
   yarnLock = ./yarn.lock;
   yarnNix = ./yarn.nix;
   buildPhase = "yarn --offline run postinstall";
+  pkgConfig = {
+    better-sqlite3 = {
+      buildInputs = [ python ];
+      postInstall = ''
+        # build native sqlite bindings
+        npm run build-release --offline --nodedir="${nodeSources}"
+      '';
+    };
+  };
 }
